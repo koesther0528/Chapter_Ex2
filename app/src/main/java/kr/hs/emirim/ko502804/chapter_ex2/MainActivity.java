fe97b7f159fb1c53d80987a4ef338623df137717
esthe <ko990727@hmail.com>
2021-04-06 19:28:14 +0900

    예제 4-18 java로 위젯속성 변경

--- app/src/main/java/kr/hs/emirim/ko502804/chapter_ex2/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/ko502804/chapter_ex2/MainActivity.java
@@ -2,13 +2,27 @@ package kr.hs.emirim.ko502804.chapter_ex2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.ex4_17);
+
+        TextView tv1 = findViewById(R.id.text1);
+        TextView tv2 = findViewById(R.id.text2);
+        TextView tv3 = findViewById(R.id.text3);
+
+        tv1.setText(R.string.helllo);
+        tv1.setTextColor(Color.RED);
+        tv2.setTextSize(30);
+        tv2.setTypeface(Typeface.SANS_SERIF, Typeface.BOLD_ITALIC);
+        tv3.setText(R.string.t5);
+        tv3.setSingleLine();
     }
 }
